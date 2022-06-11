51ae6c05a532e2e8ea44e3ac87783271c6e9e8b6
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2022-06-11 23:16:47 +0100

    props added

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     // var num1 = 0;
@@ -26,12 +27,7 @@ const Home = () => {
             <button onClick = { (e) => handleClick2('Joshua', e)}>Click Me Too</button>
             <button onClick = {increment}>Increment</button> 
             <p>{num1}</p> */}
-            {blogs.map((blog) =>(
-                <div className="blog-preview" key = {blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+         <BlogList blogs = {blogs} title = "All Blogs!"/>
         </div>
      );
 }
