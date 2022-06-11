19c97fd96d59e20e6f4d0827641d910aae252e0d
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2022-06-11 17:24:21 +0100

    added map part to the blog

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+const Home = () => {
+    // var num1 = 0;
+    // const [num, setNum] = useState(0);
+    // const handleClick = () => {
+    //     console.log("Hello Tonyo, Welcome");
+    // }
+    // const handleClick2 = (name, e) =>{
+    //     console.log("How are you "+name, e.target);
+    // }
+    // const increment = () => {
+    //     num1 += 1;
+    //     console.log(num1)
+    //     setNum(num1)
+    // }
+    const [blogs, setBlogs] = useState([
+        { title: "My New Website", body: "lorem ipsum...", author: "Tonyo", id: 1},
+        { title: "Welcome party!", body: "lorem ipsum...", author: "Michael", id: 2},
+        { title: "More web dev tips", body: "lorem ipsum...", author: "Aaron", id: 3}
+    ]);
+    return ( 
+        <div className="home">
+            {/* <h2>Homepage</h2> */}
+            {/* <button onClick = {handleClick}>Click Me</button>  
+            <button onClick = { (e) => handleClick2('Joshua', e)}>Click Me Too</button>
+            <button onClick = {increment}>Increment</button> 
+            <p>{num1}</p> */}
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key = {blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default Home;
